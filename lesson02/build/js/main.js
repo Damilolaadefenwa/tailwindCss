c90a4c3ad0e8bd3041180cb1c6d0965e4d2f8b95
damilolaadefenwa <ade24039@byui.edu>
2026-03-25 10:52:09 +0100

    added custom animation

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+// Call the function to activate it
+document.addEventListener('DOMContentLoaded', initApp)
